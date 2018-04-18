4b84a30c231de9c184fab9bf630c439e165ada58
dealsky <dealsky000@outlook.com>
2018-04-18 10:36:34 +0800

    买家类目Service

---
+++ src/main/java/com/imooc/service/CategoryService.java
@@ -0,0 +1,21 @@
+package com.imooc.service;
+
+import com.imooc.dataobject.ProductCategory;
+
+import java.util.List;
+
+/**
+ * 类目
+ * @Author: dealsky
+ * @Date: 2018/4/18 9:56
+ */
+public interface CategoryService {
+
+    ProductCategory findOne(Integer categoryId);
+
+    List<ProductCategory> findAll();
+
+    List<ProductCategory> findByCategoryTypeIn(List<Integer> categoryTypeList);
+
+    ProductCategory save(ProductCategory productCategory);
+}
