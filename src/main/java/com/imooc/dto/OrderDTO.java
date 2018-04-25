beb39a428b985d2acb799d97f68245cf3cb889b3
dealsky <dealsky000@outlook.com>
2018-04-25 11:05:42 +0800

    买家订单 service

--- src/main/java/com/imooc/dto/OrderDTO.java
+++ src/main/java/com/imooc/dto/OrderDTO.java
@@ -1,29 +1,20 @@
-package com.imooc.dataobject;
+package com.imooc.dto;
 
-import com.imooc.enums.OrderStatusEnum;
-import com.imooc.enums.PayStatusEnum;
-import lombok.Data;
-import org.hibernate.annotations.DynamicUpdate;
+import com.imooc.dataobject.OrderDetail;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
 /**
  * @Author: dealsky
- * @Date: 2018/4/24 21:31
+ * @Date: 2018/4/25 10:57
  */
-@Entity
-@Data
-@DynamicUpdate
-public class OrderMaster {
+public class OrderDTO {
 
     /**
      * 订单Id.
      */
-    @Id
     private String orderId;
 
     /**
@@ -54,12 +45,12 @@ public class OrderMaster {
     /**
      * 订单状态, 默认为0新下单
      */
-    private Integer orderStatus = OrderStatusEnum.NEW.getCode();
+    private Integer orderStatus;
 
     /**
      *支付状态, 默认0未支付
      */
-    private Integer payStatus = PayStatusEnum.WAIT.getCode();
+    private Integer payStatus;
 
     /**
      * 创建时间
@@ -70,4 +61,9 @@ public class OrderMaster {
      * 更新时间
      */
     private Date updateTime;
+
+    /**
+     * 订单详情列表
+     */
+    private List<OrderDetail> orderDetailList;
 }
