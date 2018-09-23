706d4a20b37aeff6af23c4ecdf95c78fd427ef77
dealsky <dealsky000@outlook.com>
2018-09-23 16:52:00 +0800

    微信授权

---
+++ src/main/java/com/imooc/config/WechatAccountConfig.java
@@ -0,0 +1,19 @@
+package com.imooc.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author dealsky
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "wechat")
+public class WechatAccountConfig {
+
+    private String mpAppId;
+
+    private String mpAppSecret;
+
+}
